971bb3877872f23229eeb1f2bcb16be9f54b0cc4
zozerelli <zoeapicella1@gmail.com>
2026-04-07 18:10:08 -0400

    beggining server

--- src/SimpleHTTPServer.java
+++ src/SimpleHTTPServer.java
@@ -1,2 +1,13 @@
+import java.net.ServerSocket;
 public class SimpleHTTPServer {
+
+    public static void main(String[] args) throws Exception {
+        final ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080 ....");
+        while(true){
+            //spin forever
+
+        }
+
+    }
 }
